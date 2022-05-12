ec280d7e7815891ae3efc15f81f1984b22cd8641
Александр Боровиков <borovikalex@outlook.com>
2022-05-12 13:26:02 +0300

    function

--- client/components/Single.js
+++ client/components/Single.js
@@ -3,21 +3,38 @@ import { Link, useParams } from 'react-router';
 import Photo from './Photo';
 import Comments from './Comments';
 
-const Single = React.createClass({
-  render() {
-    const {postId} = this.props.params;
-    const i = this.props.posts.findIndex(
+// const Single = React.createClass({
+//   render() {
+//     const {postId} = this.props.params;
+//     const i = this.props.posts.findIndex(
+//       (post) => post.code === postId
+//     );
+//     const post = this.props.posts[i];
+//     const postComments = this.props.comments[postId] || [];
+//     return (
+//       <div className="single-photo">
+//         <Photo i={i} post={post} {...this.props} />
+//         <Comments postComments={postComments} />
+//       </div>
+//     );
+//   },
+// }); 
+
+export function Single(props){
+   const {postId} = props.params;
+     const i = props.posts.findIndex(
       (post) => post.code === postId
     );
-    const post = this.props.posts[i];
-    const postComments = this.props.comments[postId] || [];
-    return (
+    const post = props.posts[i];
+    const postComments = props.comments[postId] || [];
+return(
+   
+
       <div className="single-photo">
-        <Photo i={i} post={post} {...this.props} />
+        <Photo i={i} post={post} {...props} />
         <Comments postComments={postComments} />
       </div>
     );
-  },
-});
+};
 
-export default Single;
+
